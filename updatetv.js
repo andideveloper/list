59acc2140e27f21e44480f0bb2447157dbc9c280
AndiSV <andideveloper.id@gmail.com>
2018-02-05 02:30:53 +0700

    Update updatetv.js

--- updatetv.js
+++ updatetv.js
@@ -1,9 +1,9 @@
 {
 	"versionCode":1,
-	"versionName":"1.2.0",
+	"versionName":"1.1.0",
 	"fileSize":"10.03MB",
 	"apkUrl":"https://github.com/andideveloper/list/raw/master/app-debug.apk",
 	"required":false,
-	"releaseDate":"2017-02-12 12:45:20",
+	"releaseDate":"2018-02-05 02:30:20",
 	"releaseNotes":["New Version", "Detail Pemperbaruan","User-friendly", "Optimalisasi Kinerja dan perbaikan Fix Bug."]
 }
