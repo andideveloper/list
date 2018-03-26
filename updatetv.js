cbc7168884f79f83740f5f3b49fb06dce9397dca
AndiSV <andideveloper.id@gmail.com>
2018-03-27 05:23:02 +0700

    Update updatetv.js

--- updatetv.js
+++ updatetv.js
@@ -2,7 +2,7 @@
 	"versionCode":2,
 	"versionName":"1.0",
 	"fileSize":"15.939MB",
-	"apkUrl":"https://github.com/andideveloper/list/raw/master/app-armeabi-v7a-release.apk"
+	"apkUrl":"https://github.com/andideveloper/list/raw/master/app-armeabi-v7a-release.apk",
 	"required":true,
 	"releaseDate":"2018-03-27 05:18:50",
 	"releaseNotes":["New Version", "Detail Pemperbaruan","User-friendly", "Optimalisasi Kinerja dan perbaikan Fix Bug.", "Support Android 7.0"]
