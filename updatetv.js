6c14405e7c991931099186acfb2e02e2f2a20ab3
AndiSV <andideveloper.id@gmail.com>
2018-12-02 03:57:48 +0700

    Update updatetv.js

--- updatetv.js
+++ updatetv.js
@@ -5,7 +5,7 @@
 	"apkUrl":"https://github.com/ngawidev/list/raw/master/app-release.apk",
 	"required":true,
 	"releaseDate":"2018-03-27 05:18:50",
-	"releaseNotes":["NEW VERSION", 
+	"releaseNotes":["  (NEW VERSION) ", 
 			"- Update New Release",
     			"- Fix Mediaplayer Android KitKat",
     			"- Update UI Player Screen",
