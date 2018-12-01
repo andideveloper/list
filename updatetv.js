911d817ca969f520517b88da5db5a4e5d802e7e1
AndiSV <andideveloper.id@gmail.com>
2018-12-02 03:52:12 +0700

    Update updatetv.js

--- updatetv.js
+++ updatetv.js
@@ -1,9 +1,13 @@
 {
 	"versionCode":2,
-	"versionName":"1.0",
+	"versionName":"1.1",
 	"fileSize":"18.3MB",
 	"apkUrl":"https://github.com/ngawidev/list/raw/master/app-release.apk",
 	"required":true,
 	"releaseDate":"2018-03-27 05:18:50",
-	"releaseNotes":["New Version", "Detail Pemperbaruan","User-friendly", "Optimalisasi Kinerja dan perbaikan Fix Bug.", "Support Android 7.0"]
+	"releaseNotes":["NEW VERSION", 
+			"- Update New Release",
+    			"- Fix Mediaplayer Android KitKat",
+    			"- Update UI Player Screen",
+    			"- Material Design"]
 }
